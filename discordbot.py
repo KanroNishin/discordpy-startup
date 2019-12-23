73f69f3fd017f011da370957407159f98bff5ff3
KanroNishin <59179711+KanroNishin@users.noreply.github.com>
2019-12-24 06:21:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,52 +1,26 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
 
-from func import diceroll
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN =  os.environ['DISCORD_BOT_TOKEN']
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
+# 起動時に動作する処理
 @client.event
 async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
 
 # メッセージ受信時に動作する処理
-
 @client.event
 async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-#ねこ移植
-    if message.content.startswith("/neko"):
-         await message.channel.send('にゃーん')
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-    if message.content.startswith("/dice"):
-        # 入力された内容を受け取る
-        say = message.content 
-
-        # [!dice ]部分を消し、AdBのdで区切ってリスト化する
-        order = say.strip('/dice ')
-        cnt, mx = list(map(int, order.split('d'))) # さいころの個数と面数
-        dice = diceroll(cnt, mx) # 和を計算する関数(後述)
-        await message.channel.send(dice[cnt])
-        del dice[cnt]
-
-        # さいころの目の総和の内訳を表示する
-        await message.channel.send(dice)
-    
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
