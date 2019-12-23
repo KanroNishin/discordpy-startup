2d28e99697c30c931b2c3733c4ba89a8178b1fef
KanroNishin <59179711+KanroNishin@users.noreply.github.com>
2019-12-24 06:57:01 +0900

    Create index.py

---
+++ index.py
@@ -0,0 +1,26 @@
+import discord
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'NjU2NjE4NTkyNDM1MTc1NDQ0.XgEuuw.5NfCJ9ZwwlpeJUfv1gG24o6s_4w'
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
